38304374685ed3034a5c0e7057e12e42c967e867
gapry <gapry@protonmail.com>
2026-03-08 16:28:11 +0800

    nvim-tree: update

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -35,13 +35,13 @@ return {
     vim.api.nvim_create_autocmd("VimEnter", {
       callback = function(data)
         if not data.file or data.file == "" then
-          vim.notify("Nvim-tree: No target file specified.", vim.log.levels.INFO)
+          require("nvim-tree.api").tree.open()
           return
         end
 
         local directory = vim.fn.isdirectory(data.file) == 1
 
-        if directory then
+        if directory and vim.loop.fs_access(data.file, "R") then
           local ok, err = pcall(vim.api.nvim_set_current_dir, data.file)
           if ok then
             require("nvim-tree.api").tree.open()
@@ -52,7 +52,7 @@ return {
       end,
     })
 
-    vim.keymap.set("n", "<leader>e" , "<cmd>NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
+    vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
     vim.keymap.set("n", "<leader>fe", "<cmd>NvimTreeFocus<CR>", { desc = "Focus File Explorer" })
   end,
 }
