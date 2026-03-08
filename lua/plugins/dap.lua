214ebf84362286bc1eb31e24ac7caa981668a1df
gapry <gapry@protonmail.com>
2026-03-08 18:00:43 +0800

    dap: init (#11)

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,53 @@
+return {
+  "mfussenegger/nvim-dap",
+
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    "jay-babu/mason-nvim-dap.nvim",
+  },
+
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+    local vscode = require("dap.ext.vscode")
+
+    require("mason-nvim-dap").setup({
+      ensure_installed = { 
+        "codelldb",
+        "debugpy"
+      },
+
+      automatic_installation = true,
+
+      handlers = {
+        function(config)
+          require('mason-nvim-dap').default_setup(config)
+        end,
+      },
+    })
+
+    dapui.setup()
+
+    vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
+    vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
+    vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
+    vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" })
+    vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+
+    dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+    dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+    dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+
+    vscode.load_launchjs(nil, {
+      codelldb = {
+        "c",
+        "cpp",
+        "rust"
+      },
+      debugpy = {
+        "python"
+      },
+    })
+  end,
+}
