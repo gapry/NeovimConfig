9827e4e26f0f5ed821996231295a1b3a2ed4a9e2
gapry <gapry@protonmail.com>
2026-03-08 15:43:27 +0800

    treesitter: rename configs to config following upstream change (#5)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,19 +3,20 @@ return {
 
   build = ":TSUpdate",
 
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      -- https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
-      ensure_installed = { 
-        "cmake", "make",
-        "asm", "c", "cpp", "rust", "go", "zig",
-        "nix", "haskell", "scala",
-        "python", "lua", "bash",
-        "vim", "vimdoc" 
-      },
-      highlight = { 
-        enable = true 
-      },
-    })
-  end,
+  -- https://github.com/nvim-lua/kickstart.nvim/pull/1748/changes
+  main = "nvim-treesitter.config",
+
+  opts = {
+    -- https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
+    ensure_installed = {
+      "cmake", "make",
+      "asm", "c", "cpp", "rust", "go", "zig",
+      "nix", "haskell", "scala",
+      "python", "lua", "bash",
+      "vim", "vimdoc"
+    },
+    highlight = {
+      enable = true,
+    },
+  },
 }
