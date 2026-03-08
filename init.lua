b954bac7cd555ffd4f76cacb3638c3a585788533
gapry <gapry@protonmail.com>
2026-03-08 14:39:54 +0800

    lazy: init (#3)

--- init.lua
+++ init.lua
@@ -12,3 +12,5 @@ opt.encoding = "utf-8"
 opt.fileencoding = "utf-8"
 opt.fileencodings = { "utf-8", "ucs-bom", "latin1", "cp1252" }
 
+require("config.lazy")
+
---
+++ lua/config/lazy.lua
@@ -0,0 +1,27 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  local result = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+
+  if vim.v.shell_error ~= 0 then
+    vim.notify("Failed to clone lazy.nvim: " .. result, vim.log.levels.ERROR)
+    return
+  end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local ok, lazy = pcall(require, "lazy")
+if not ok then
+  vim.notify("Failed to load lazy.nvim", vim.log.levels.ERROR)
+  return
+end
+lazy.setup("plugins")
+
