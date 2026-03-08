07c26eaac1cd7ceabb1751cc1930df57593c76b7
gapry <gapry@protonmail.com>
2026-03-08 16:38:12 +0800

    settings: support window switch

--- init.lua
+++ init.lua
@@ -12,5 +12,10 @@ opt.encoding = "utf-8"
 opt.fileencoding = "utf-8"
 opt.fileencodings = { "utf-8", "ucs-bom", "latin1", "cp1252" }
 
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+
 require("config.lazy")
 
