1b2d66b814f133d97bb7914796fa080f0f733e9a
gapry <gapry@protonmail.com>
2026-03-08 18:17:36 +0800

    dap: delete deprecated setting (#12)

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -38,16 +38,5 @@ return {
     dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
     dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
     dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
-
-    vscode.load_launchjs(nil, {
-      codelldb = {
-        "c",
-        "cpp",
-        "rust"
-      },
-      debugpy = {
-        "python"
-      },
-    })
   end,
 }
