e648f6cb4996e2a11b295f1666f5a70caec5816a
gapry <gapry@protonmail.com>
2026-03-08 12:04:16 +0800

    config: add basic settings

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
-print("Hello! Neovim")
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.clipboard = "unnamedplus"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.fileencodings = { "utf-8", "ucs-bom", "latin1", "cp1252" }
+
