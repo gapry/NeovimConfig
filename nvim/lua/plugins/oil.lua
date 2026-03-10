73cd12110e4148d5460939a93b74b51040cca51b
gapry <gapry@protonmail.com>
2026-03-10 22:05:29 +0800

    feat: switch file explorer to oil from nvim-tree

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,45 @@
+return {
+  "stevearc/oil.nvim",
+
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+
+  opts = {
+    default_file_explorer = true,
+
+    columns = {
+      "icon",
+      "permissions",
+      "size",
+      "mtime",
+    },
+
+    view_options = {
+      show_hidden = false,
+    },
+
+    keymaps = {
+      ["<C-s>"] = "actions.select_vsplit",
+      ["<C-h>"] = "actions.select_split",
+      ["<C-t>"] = "actions.select_tab",
+      ["<CR>"] = "actions.select",
+      ["g."] = "actions.toggle_hidden",
+      ["q"] = "actions.close",
+    },
+
+    use_default_keymaps = false,
+  },
+
+  config = function(_, opts)
+    require("oil").setup(opts)
+
+    vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
+
+    vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>", { desc = "File Explorer" })
+
+    vim.keymap.set("n", "<leader>fe", function()
+      require("oil").open(vim.fn.expand("%:p:h"))
+    end, { desc = "Explorer (current file)" })
+  end,
+}
