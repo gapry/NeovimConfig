eca67ba864dbc36e8fe5b2855b375fc0d96ea2eb
gapry <gapry@protonmail.com>
2026-03-10 16:10:17 +0800

    fzf: define navigation keymaps

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -26,5 +26,21 @@ return {
     vim.keymap.set("n", "<leader>ps", fzf.live_grep, { desc = "Grep Project" })
     vim.keymap.set("n", "<leader>pb", fzf.buffers, { desc = "Find Buffers" })
     vim.keymap.set("n", "<leader>psy", fzf.lsp_document_symbols, { desc = "LSP Symbols" })
+    vim.keymap.set("n", "<leader>psw", fzf.lsp_live_workspace_symbols, { desc = "LSP Workspace Symbols" })
+
+    vim.keymap.set("n", "gd", function()
+      require("fzf-lua").lsp_definitions({ jump_to_single_result = true })
+    end, { desc = "Jump to Definition" })
+
+    vim.keymap.set("n", "gb", "<C-o>", { desc = "Jump Back" })
+    vim.keymap.set("n", "gf", "<C-i>", { desc = "Jump Forward" })
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function()
+        vim.schedule(function()
+          vim.cmd("normal! zz")
+        end)
+      end,
+    })
   end,
 }
