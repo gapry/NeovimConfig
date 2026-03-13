8e927c5e9044414b45bfb7f843fc44677596bdee
gapry <gapry@protonmail.com>
2026-03-13 11:43:46 +0800

    hotkey: always save and quit all on exit (#31)

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,6 +17,8 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
 
+vim.keymap.set("n", "<leader>q", "<cmd>wqa<CR>", { desc = "Exit: Save and quit all" })
+
 vim.api.nvim_create_autocmd("BufReadPost", {
   callback = function()
     local last_pos = vim.api.nvim_buf_get_mark(0, '"')
