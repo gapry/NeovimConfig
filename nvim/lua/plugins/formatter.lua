c1ef0a0c9eb55e9c753de887bea8bffdc385cf3e
gapry <gapry@protonmail.com>
2026-03-28 13:31:01 +0800

    formatter: update setting (#34)

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -1,5 +1,6 @@
 return {
   "stevearc/conform.nvim",
+
   opts = {
     formatters_by_ft = {
       c = { "clang_format" },
@@ -7,9 +8,22 @@ return {
       rust = { "rustfmt" },
       lua = { "stylua" },
     },
-    format_on_save = {
-      timeout_ms = 500,
+
+    format_on_save = false,
+
+    format = {
       lsp_fallback = true,
+      async = true,
+      timeout_ms = 500,
     },
   },
+
+  config = function(_, opts)
+    local conform = require("conform")
+    conform.setup(opts)
+
+    vim.keymap.set({ "n", "v" }, "<leader>f", function()
+      conform.format(opts.format)
+    end, { desc = "Format buffer" })
+  end,
 }
