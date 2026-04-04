c2c52903c03960a842cb1d1ebfda15edf4608385
gapry <gapry@protonmail.com>
2026-04-04 20:48:56 +0800

    lsp: code refactoring (#44)

---
+++ nvim/lsp/rust_analyzer.lua
@@ -0,0 +1,13 @@
+return {
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      },
+      check = {
+        command = "clippy",
+      },
+      checkOnSave = true,
+    },
+  },
+}
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -45,6 +45,8 @@ return {
 
     dependencies = {
       "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+      "saghen/blink.cmp",
     },
 
     config = function()
@@ -55,99 +57,22 @@ return {
           "pyright",
           "texlab",
         },
+        automatic_enable = true,
       })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
 
-    dependencies = {
-      "saghen/blink.cmp",
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-    },
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local opts = { buffer = args.buf, noremap = true, silent = true }
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+        end,
+      })
 
-    config = function()
-      local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-      local on_attach = function(_, bufnr)
-        local opts = {
-          buffer = bufnr,
-          noremap = true,
-          silent = true,
-        }
-
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-      end
-
-      local servers = {
-        "clangd",
-        "rust_analyzer",
-        "pyright",
-        "texlab",
-      }
-
-      local server_configs = {
-        rust_analyzer = {
-          settings = {
-            ["rust-analyzer"] = {
-              cargo = {
-                allFeatures = true,
-              },
-              check = {
-                command = "clippy",
-              },
-              checkOnSave = true,
-            },
-          },
-        },
-        texlab = {
-          settings = {
-            texlab = {
-              build = {
-                onSave = true,
-                forwardSearchAfter = true,
-                executable = "latexmk",
-                args = {
-                  "-pdf",
-                  "-interaction=nonstopmode",
-                  "-synctex=1",
-                  "%f",
-                },
-              },
-              forwardSearch = {
-                executable = "zathura",
-                args = {
-                  "--synctex-forward",
-                  "%l:1:%f",
-                  "%p",
-                },
-              },
-              chktex = {
-                onOpenAndSave = true,
-              },
-            },
-          },
-        },
-      }
-
-      for _, server in ipairs(servers) do
-        local opts = {
-          on_attach = on_attach,
-          capabilities = capabilities,
-        }
-
-        if server_configs[server] then
-          opts = vim.tbl_deep_extend("force", opts, server_configs[server])
-        end
-
-        vim.lsp.config(server, opts)
-      end
-
-      vim.lsp.enable(servers)
+      vim.lsp.config("*", {
+        capabilities = require("blink.cmp").get_lsp_capabilities(),
+      })
     end,
   },
 }
