7b3f1e6aa50f4efcf4dcbfbd9a27505adda899bd
gapry <gapry@protonmail.com>
2026-04-04 08:44:21 +0800

    Makefile: force tab indentation and its width

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,4 +28,14 @@ vim.api.nvim_create_autocmd("BufReadPost", {
   end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "make",
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+  end,
+})
+
 require("config.lazy")
+
