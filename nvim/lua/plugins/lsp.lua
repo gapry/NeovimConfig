cfe81b81d68f3e92b1fbfad8c732e555f58db3d0
gapry <gapry@protonmail.com>
2026-04-04 09:32:04 +0800

    keymap: remove duplication (#38)

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -77,7 +77,6 @@ return {
           silent = true,
         }
 
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
