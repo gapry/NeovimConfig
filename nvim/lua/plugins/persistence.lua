202bbe737e657eb804b97183e9fdaffcb5aa5c73
gapry <gapry@protonmail.com>
2026-04-29 19:21:38 +0800

    persistence: code formatting

--- nvim/lua/plugins/persistence.lua
+++ nvim/lua/plugins/persistence.lua
@@ -3,8 +3,7 @@ return {
 
   event = "BufReadPre",
 
-  opts = {
-  },
+  opts = {},
 
   config = function(_, opts)
     local persistence = require("persistence")
@@ -15,13 +14,23 @@ return {
       callback = function()
         if vim.fn.argc() == 1 and vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
           vim.cmd("cd " .. vim.fn.fnameescape(vim.fn.argv(0)))
-          vim.schedule(function() persistence.load() end)
+          vim.schedule(function()
+            persistence.load()
+          end)
         end
       end,
     })
 
-    vim.keymap.set("n", "<leader>ss", function() persistence.load() end, { desc = "Restore Session" })
-    vim.keymap.set("n", "<leader>sl", function() persistence.load({ last = true }) end, { desc = "Restore Last Session" })
-    vim.keymap.set("n", "<leader>sd", function() persistence.stop() end, { desc = "Don't Save Current Session" })
+    vim.keymap.set("n", "<leader>ss", function()
+      persistence.load()
+    end, { desc = "Restore Session" })
+
+    vim.keymap.set("n", "<leader>sl", function()
+      persistence.load({ last = true })
+    end, { desc = "Restore Last Session" })
+
+    vim.keymap.set("n", "<leader>sd", function()
+      persistence.stop()
+    end, { desc = "Don't Save Current Session" })
   end,
 }
