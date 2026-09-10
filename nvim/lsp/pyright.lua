72bb83c122d1198690cdfa9fda49ea565f6b455e
gapry <gapry@protonmail.com>
2026-09-10 14:54:00 +0800

    pyright: auto-detect uv venv

--- nvim/lsp/pyright.lua
+++ nvim/lsp/pyright.lua
@@ -1,1 +1,15 @@
-return {}
+return {
+  before_init = function(_, config)
+    if not config.root_dir then
+      return
+    end
+
+    local venv_python = config.root_dir .. "/.venv/bin/python"
+
+    if vim.uv.fs_stat(venv_python) then
+      config.settings = config.settings or {}
+      config.settings.python = config.settings.python or {}
+      config.settings.python.pythonPath = venv_python
+    end
+  end,
+}
